9bc4313ba05976f5498e026f6492190b8d97d748
Rafael Coelho <rafaeld3v@gmail.com>
2022-06-20 15:40:28 -0300

    Projeto finalizado

---
+++ src/assets/svg icons/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import loadingSvg from '../images/Spinner.svg'
+
+function Loading() {
+
+  return (
+    <>
+      <img src={loadingSvg}></img>
+    </>
+  )
+}
+
+export default Loading
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,18 +1,63 @@
 import React from 'react';
 import IconDice from '../../assets/svg icons/IconDice';
+import Loading from '../../assets/svg icons/Loading';
 import PatternDividerMobile from '../../assets/svg icons/PatternDividerMobile';
+import PatternDividerDesktop from '../../assets/svg icons/PatternDividerDesktop';
 import './Card.css';
 
 function Card() {
+
+  const url = 'https://api.adviceslip.com/advice';
+  const [advice, setAdvice] = React.useState(null);
+
+
+  React.useEffect(() => {
+    async function initialAdvice() {
+
+      let result = await fetch(`${url}`);
+      let {slip} = await result.json();
+  
+      setAdvice({ adviceNumber: slip.id, adviceReturn: slip.advice })
+    }
+
+    initialAdvice();
+  }, [])
+
+
+  async function newAdvice() {
+
+    let result = await fetch(`${url}`);
+    let {slip} = await result.json();
+
+    setAdvice({ adviceNumber: slip.id, adviceReturn: slip.advice })
+  }
+
+  
   return (
-    <div className='card-container'>
-      <span>Advice #44</span>
-      <p>If you are feeling down, try holding a pencil between your top lip and your nose for five minutes.</p>
-      <PatternDividerMobile />
-      <button className='next-button'>
-        <IconDice />
-      </button>
-    </div>
+    <>
+      {
+        (advice) ?
+
+          <div className='card-container'>
+            <span className='advice-number'>
+              {(advice) && `ADVICE #${advice.adviceNumber}`}
+            </span>
+            <q className='advice-text'>
+              {(advice) && advice.adviceReturn}
+            </q>
+            <PatternDividerMobile />
+            <PatternDividerDesktop />
+            <button className='next-button' onClick={newAdvice}>
+            <IconDice />
+            </button>
+          </div>
+
+        :
+
+          <Loading />
+      }
+    </>
+    
   )
 }
 
